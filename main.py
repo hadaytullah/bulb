f4ac381b129aa987d1c7e20aa456adb781207caf
hadaytullah <hadaytullah@gmail.com>
2018-10-04 20:31:07 +0300

    creative home and awarenesses(TODO)

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from smart_home import SmartHome
 from control_home import ControlHome
+from creative_home import CreativeHome
 
 WIDTH = 20
 HEIGHT = 20
 
-control_home = ControlHome(WIDTH,HEIGHT)
-control_home.run()
+#control_home = ControlHome(WIDTH,HEIGHT)
+#control_home.run()
 
 #smart_home = SmartHome(WIDTH,HEIGHT)
 #smart_home.run()
+
+creative_home = CreativeHome(WIDTH,HEIGHT)
+creative_home.run()
