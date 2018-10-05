f237e8333b1f3298a24955bd616349dcd30d2dd8
hadaytullah <hadaytullah@gmail.com>
2018-10-05 18:05:24 +0300

    self adaptive home with single adaptive strategy added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from smart_home import SmartHome
 from control_home import ControlHome
 from creative_home import CreativeHome
+from adaptive_home import AdaptiveHome
 
 WIDTH = 20
 HEIGHT = 20
@@ -11,5 +12,8 @@ HEIGHT = 20
 #smart_home = SmartHome(WIDTH,HEIGHT)
 #smart_home.run()
 
-creative_home = CreativeHome(WIDTH,HEIGHT)
-creative_home.run()
+#creative_home = CreativeHome(WIDTH,HEIGHT)
+#creative_home.run()
+
+adaptive_home = AdaptiveHome(WIDTH, HEIGHT)
+adaptive_home.run()
